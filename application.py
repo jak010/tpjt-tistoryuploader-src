83d3bf2fb47be4b8ce791cb372c9b26ffdb88c37
jak010 <bluetoon@naver.com>
2024-09-23 21:00:24 +0900

    bugfix6

--- application.py
+++ application.py
@@ -74,7 +74,7 @@ if __name__ == '__main__':
         ),
         tistory_oauth_executer=OauthLoginExecuter(
             driver=ChromeDriver.build(
-                executable_path=None,
+                executable_path="/usr/local/bin/chromedriver",
                 option=ChromeOption(
                     window_size_width=780,
                     window_size_height=620,
